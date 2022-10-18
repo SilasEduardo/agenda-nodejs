fbc6e906e61ac55e541d72a392a8f4de936d5b31
Silas de Paula <silasandrade94@gmail.com>
2022-10-18 15:33:14 -0300

    atualização

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,4 +1,12 @@
+const Login = require('../models/LoginModel')
+
 exports.index = (req, res) => {
     res.render('login');
    return
+};
+
+exports.register = (req, res) => {
+   const login = new Login(req.body)
+   login.register();
+   res.send(login.errors);
 };
