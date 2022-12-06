c0bacd4e19b82ea56bf74a1d8e91efbcafa9c98b
SilasEduardo <silasandrade94@gmail.com>
2022-12-05 22:29:09 -0300

    login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,4 +1,16 @@
+const Login = require('../models/Login')
+
 exports.index = (req, res) => {
     res.render('login');
     return;
   };
+
+
+exports.register = (req, res) => {
+
+  const { ...body } = req.body
+
+  const login = new Login(body)
+
+  res.send(body.email)
+};
