e8eafdc4e1cd3d5098051ae5e3181ce5c8f6bff9
SilasEduardo <silasandrade94@gmail.com>
2022-12-08 21:13:44 -0300

    registration

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,4 @@
+const { async } = require('regenerator-runtime');
 const Login = require('../models/Login')
 
 exports.index = (req, res) => {
@@ -6,10 +7,34 @@ exports.index = (req, res) => {
   };
 
 
-exports.register = (req, res) => {
+exports.register = async (req, res) => {
+  try{
+    const { ...body } = req.body;
+    const  login = new Login(body);
+    await login.register();
 
-  const { ...body } = req.body;
-  const login = new Login(body);
+    
+  
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+       return res.redirect('index');
 
-  res.send(login.body)
+      })
+      
+      return
+    };
+
+    req.flash('success', 'user successfully savid');
+      req.session.save(function() {
+       return res.redirect('index');
+
+      })
+    
+  } catch (e) {
+    res.render('404')
+    console.error(e)
+  }
+
+ 
 };
